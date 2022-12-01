bf62d4593d9ed952bdd9c02745e072419d5ceef0
Jonny Klasson <jonny.klasson@else.se>
2022-12-01 06:56:53 +0100

    Day1

--- ConsoleApp/Callendar/D01/Part1.cs
+++ ConsoleApp/Callendar/D01/Part1.cs
@@ -4,8 +4,26 @@
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileAsync("Test");
-            throw new NotFiniteNumberException();
+            var input = await ReadFileAsync("Input");
+            var kalories = new Dictionary<int, int> { [0] = 0 };
+            int index = 0;
+            foreach (var kal in input)
+            {
+                if (kal.Length == 0)
+                {
+                    index++;
+                    kalories.Add(index, 0);
+                    continue;
+                }
+
+                kalories[index] += int.Parse(kal);
+            }
+
+            return kalories
+                .Values
+                .OrderDescending()
+                .Select(x => x.ToString())
+                .First();
         }
     }
 }
--- ConsoleApp/Callendar/D01/Part2.cs
+++ ConsoleApp/Callendar/D01/Part2.cs
@@ -4,8 +4,27 @@
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileAsync("Test");
-            throw new NotFiniteNumberException();
+            var input = await ReadFileAsync("Input");
+            var kalories = new Dictionary<int, int> { [0] = 0 };
+            int index = 0;
+            foreach (var kal in input)
+            {
+                if (kal.Length == 0)
+                {
+                    index++;
+                    kalories.Add(index, 0);
+                    continue;
+                }
+
+                kalories[index] += int.Parse(kal);
+            }
+
+            return kalories
+                .Values
+                .OrderDescending()
+                .Take(3)
+                .Sum()
+                .ToString();
         }
     }
 }
