71f239286b2c5cad1fa912a43319cd86bd20f7f9
Jonny Klasson <jonny.klasson@else.se>
2022-12-03 06:32:12 +0100

    Day3

--- ConsoleApp/Callendar/D03/Part1.cs
+++ ConsoleApp/Callendar/D03/Part1.cs
@@ -4,8 +4,21 @@
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileAsync("Test");
-            throw new NotFiniteNumberException();
+            var input = await ReadFileAsync("Input");
+            var points = Enumerable.Range('a', 'z' - 'a' + 1)
+                .Union(Enumerable.Range('A', 'Z' - 'A' + 1))
+                .Select((x, i) => new { Value = (char)x, Score = i + 1 })
+                .ToDictionary(x => x.Value, x => x.Score);
+
+            var scores = 0;
+            foreach (var ruggsack in input)
+            {
+                var comp1 = ruggsack.Take(ruggsack.Length / 2).ToList();
+                var comp2 = ruggsack.Skip(comp1.Count).ToList();
+                var match = comp1.Intersect(comp2).ToList();
+                scores += match.Sum(x => points[x]);
+            }
+            return scores.ToString();
         }
     }
 }
--- ConsoleApp/Callendar/D03/Part2.cs
+++ ConsoleApp/Callendar/D03/Part2.cs
@@ -4,8 +4,22 @@
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileAsync("Test");
-            throw new NotFiniteNumberException();
+            var input = await ReadFileAsync("Input");
+            var points = Enumerable.Range('a', 'z' - 'a' + 1)
+                .Union(Enumerable.Range('A', 'Z' - 'A' + 1))
+                .Select((x, i) => new { Value = (char)x, Score = i + 1 })
+                .ToDictionary(x => x.Value, x => x.Score);
+
+            var scores = 0;
+            foreach (var group in input.Select((x, i) => new {Value = x, Index = i}).GroupBy(x => x.Index / 3))
+            {
+                var first = group.First().Value;
+                var second = group.Skip(1).First().Value;
+                var thrid = group.Last().Value;
+                var badge = first.Intersect(second).Intersect(thrid);
+                scores += badge.Sum(x => points[x]);
+            }
+            return scores.ToString();
         }
     }
 }
