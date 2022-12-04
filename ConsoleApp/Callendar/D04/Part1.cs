3b477b5a3033f0ea2633a31c3f57e3af68330d1d
Jonny Klasson <jonny.klasson@else.se>
2022-12-04 08:46:32 +0100

    Day4

--- ConsoleApp/Callendar/D04/Part1.cs
+++ ConsoleApp/Callendar/D04/Part1.cs
@@ -4,8 +4,22 @@
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotFiniteNumberException();
+            var input = await ReadFileLinesAsync("Input");
+            var overLaps = 0;
+            foreach (var pair in input.Select(x
+                         => x.Split(',')
+                             .Select(y
+                                 => y.Split('-').Select(int.Parse).ToArray())
+                             .Select(y => new { Start = y[0], End = y[1] })
+                             .ToArray()))
+            {
+                if (pair[0].Start <= pair[1].Start && pair[0].End >= pair[1].End)
+                    overLaps++;
+                else if (pair[1].Start <= pair[0].Start && pair[1].End >= pair[0].End)
+                    overLaps++;
+            }
+
+            return overLaps.ToString();
         }
     }
 }
