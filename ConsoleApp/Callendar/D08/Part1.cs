091480acfdc4555df38095487a3bcd2c83c899cc
Jonny Klasson <jonny.klasson@else.se>
2022-12-09 06:59:02 +0100

    Day8

--- ConsoleApp/Callendar/D08/Part1.cs
+++ ConsoleApp/Callendar/D08/Part1.cs
@@ -1,11 +1,39 @@
-﻿namespace ConsoleApp.Callendar.D08
+﻿using System.Drawing;
+
+namespace ConsoleApp.Callendar.D08
 {
     internal class Part1 : Part
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotFiniteNumberException();
+            var input = await ReadFileLinesAsync("Input");
+            var grid = input.Select((r, iy) => r.Select((c, ix) => new Three(ix, iy, int.Parse(c.ToString())))
+                    .ToArray()
+                ).ToArray();
+            var visible = 0;
+            for (int y = 0; y < grid.Length; y++)
+            {
+                for (int x = 0; x < grid[y].Length; x++)
+                {
+                    var three = grid[y][x];
+                    if (y == 0 || x == 0)
+                        visible++;
+                    else if (y == grid.Length || x == grid[y].Length)
+                        visible++;
+                    else if (grid.Take(y).All(col => col[x].Size < three.Size)) // Upp
+                        visible++;
+                    else if (grid[y].Where(row => row.X < three.X).All(row => row.Size < three.Size)) // Left
+                        visible++;
+                    else if (grid.Skip(y + 1).All(col => col[x].Size < three.Size)) // down
+                        visible++;
+                    else if (grid[y].Where(row => row.X > three.X).All(row => row.Size < three.Size)) // Right
+                        visible++;
+                }
+            }
+
+            return visible.ToString();
         }
+
+        internal record Three(int X, int Y, int Size);
     }
 }
--- ConsoleApp/Callendar/D08/Part2.cs
+++ ConsoleApp/Callendar/D08/Part2.cs
@@ -4,8 +4,66 @@
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotFiniteNumberException();
+            var input = await ReadFileLinesAsync("Input");
+            var edge = new { X = input.Length - 1, Y = input.Length - 1 };
+            var grid = input.SelectMany((r, iy) => r.Select((c, ix)
+                    => new Three(ix, iy, int.Parse(c.ToString()), ix == 0 || iy == 0 || ix == edge.X || iy == edge.Y)))
+                .ToList();
+
+            return grid.Select(x => x.Calculate(grid))
+                .OrderByDescending(x => x.Score)
+                .First().Score
+                .ToString();
+        }
+
+        internal record Three(int X, int Y, int Size, bool Edge)
+        {
+            public int Upp { get; set; }
+            public int Down { get; set; }
+            public int Left { get; set; }
+            public int Right { get; set; }
+            public int Score => Upp * Left * Down * Right;
+
+            public Three Calculate(List<Three> grid)
+            {
+                if (Edge)
+                    return this;// always 0
+
+                var row = grid.Where(x => x.X == X).ToList();
+                Upp = row.Where(x => x.Y < Y)
+                    .OrderByDescending(x => x.Y)
+                    .TakeUntil(x => x.Size >= Size)
+                    .Count();
+                Down = row.Where(x => x.Y > Y)
+                    .OrderBy(x => x.Y)
+                    .TakeUntil(x => x.Size >= Size)
+                    .Count();
+
+                var column = grid.Where(x => x.Y == Y).ToList();
+                Left = column.Where(x => x.X < X)
+                    .OrderByDescending(x => x.X)
+                    .TakeUntil(x => x.Size >= Size)
+                    .Count();
+                Right = column.Where(x => x.X > X)
+                    .OrderBy(x => x.X)
+                    .TakeUntil(x => x.Size >= Size)
+                    .Count();
+
+                return this;
+            }
+        }
+    }
+
+    internal static class Extension
+    {
+        public static IEnumerable<T> TakeUntil<T>(this IEnumerable<T> list, Func<T, bool> predicate)
+        {
+            foreach (var item in list)
+            {
+                yield return item;
+                if (predicate.Invoke(item))
+                    yield break;
+            }
         }
     }
 }
