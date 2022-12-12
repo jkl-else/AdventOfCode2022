1dc4b80c6477d113b134115ef2cfa35cfcdcd663
Jonny Klasson <jonny.klasson@else.se>
2022-12-12 19:17:55 +0100

    Day10

--- ConsoleApp/Callendar/D10/Part1.cs
+++ ConsoleApp/Callendar/D10/Part1.cs
@@ -2,10 +2,39 @@
 {
     internal class Part1 : Part
     {
+        internal List<int> Signals {get; } = new List<int>();
+        internal int Cycles
+        {
+            get => _cycles;
+            set
+            {
+                _cycles = value;
+                if (_cycles == 20 || (_cycles - 20) % 40 == 0)
+                    Signals.Add(_cycles * X);
+            } }
+        private int _cycles;
+        internal int X { get; set; } = 1;
+
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotFiniteNumberException();
+            var input = await ReadFileLinesAsync("Input");
+            foreach (var data in input)
+            {
+                if (data == "noop")
+                {
+                    Cycles++;
+                    continue;
+                }
+
+                for (int i = 0; i < 2; i++)
+                {
+                    Cycles++;
+                }
+
+                X += int.Parse(data.Split(' ')[1]);
+            }
+
+            return Signals.Sum().ToString();
         }
     }
 }
--- ConsoleApp/Callendar/D10/Part2.cs
+++ ConsoleApp/Callendar/D10/Part2.cs
@@ -2,10 +2,47 @@
 {
     internal class Part2 : Part
     {
+        internal int Cycles
+        {
+            get => _cycles;
+            set
+            {
+                if (_cycles == 40)
+                {
+                    Console.WriteLine();
+                    _cycles = 1;
+                    Console.Write(Math.Abs(X - _cycles) <= 1 ? "#" : ".");
+                }
+                else
+                {
+                    Console.Write(Math.Abs(X - _cycles) <= 1 ? "#" : ".");
+                    _cycles = value;
+                }
+            }
+        }
+        private int _cycles;
+        internal int X { get; set; } = 1;
+
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotFiniteNumberException();
+            var input = await ReadFileLinesAsync("Input");
+            foreach (var data in input)
+            {
+                if (data == "noop")
+                {
+                    Cycles++;
+                    continue;
+                }
+
+                for (int i = 0; i < 2; i++)
+                {
+                    Cycles++;
+                }
+
+                X += int.Parse(data.Split(' ')[1]);
+            }
+
+            return "";
         }
     }
 }
